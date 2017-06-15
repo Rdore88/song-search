7a77a31dc88661badca684c7e9bf745f2679fb35
Robby Dore <rdore88@gmail.com>
2017-06-15 13:40:44 -0400

    Initial setup and bootstrp.

---
+++ webchat.js
@@ -0,0 +1,10 @@
+console.log("Webchat client is running ...");
+
+function test() {
+	console.log("Testing ...");
+}
+
+function sendMessage() {
+	var userMessage = document.getElementById('user-message').value;
+	console.log("About to send " + userMessage);
+}
